3a13b6c96be716fba504a3138d4d7470b534c3b3
Manivasagam Suresh <mani@ayoo.care>
2024-06-23 19:31:35 +0530

    basic api and mongo connection completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const getAllProducts = async () => {
+    try {
+      const response = await axios.get("/api/productsAPi");
+      console.log("GET response:", response.data);
+    } catch (error) {
+      console.error("Error calling GET API:", error);
+    }
+  };
+
+    const postproduct = async () => {
+      try {
+        const response = await axios.post("/api/productsAPi",{
+          "name": "Organic Apple",
+          "price": 2.99,
+          "category": "Fruits",
+          "weight_in_grams": 150
+        }
+        );
+        console.log("POST response:", response.data);
+      } catch (error) {
+        console.error("Error calling GET API:", error);
+      }
+    };
+
+
   return (
     <div className="w-full flex justify-center">
-    <div className="text-lg text-center flex justify-center items-center min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-104px)] xl:min-h-[calc(100vh-120px)] w-full max-w-[1850px]">
-      Hello All, Welcome to Kavi Seyon foods
-    </div>
+      <div className="text-lg text-center flex flex-col justify-center items-center gap-4 min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-104px)] xl:min-h-[calc(100vh-120px)] w-full max-w-[1850px]">
+      <span> Hello All, Welcome to Kavi Seyon foods </span>  <button onClick={postproduct}>POST</button> <button onClick={getAllProducts}>GET</button> 
+      </div>
+      
     </div>
   );
 }
