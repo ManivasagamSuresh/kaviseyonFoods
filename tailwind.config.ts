b7f8199451a7077d4e327a3045033b9aa2b5d0e9
Manivasagam Suresh <mani@ayoo.care>
2024-06-27 16:48:25 +0530

    change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@
 const { Config } = require("tailwindcss");
 
 const config = {
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
