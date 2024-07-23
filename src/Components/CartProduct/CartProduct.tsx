9ed4f6be988b32ddcce4630c2163cf10090dd6fa
Manivasagam Suresh <mani@ayoo.care>
2024-07-23 18:13:59 +0530

    cart UI done

--- src/Components/CartProduct/CartProduct.tsx
+++ src/Components/CartProduct/CartProduct.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { LiaRupeeSignSolid } from "react-icons/lia";
 // import { FaMinus } from "react-icons/fa6";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 interface ProductComponentProps {
   prod: CartProduct;
@@ -15,32 +16,43 @@ const CartProduct: React.FC<ProductComponentProps> = ({ prod }) => {
 
   return (
     <div className="flex my-2 lg:my-4">
-      <div className="w-7/12 flex gap-3 lg:gap-6">
-        <div className="w-24 h-28 md:h-44 md:w-36 2xl:w-44 2xl:h-52 relative">
+      <div className="w-8/12 md:w-6/12 lg:w-7/12 flex gap-3 lg:gap-6">
+        <div className="w-20 h-24 lg:w-16 lg:h-24 2xl:w-32 2xl:h-40 relative">
           <Image
             src={getImageSrc(prod.image)}
             alt="Product Image"
             fill={true} // Ensures the Image fills its container
           />
         </div>
-        <div className="flex flex-col">
-          <div className="text-sm md:text-base xl:text-lg 2xl:text-xl">{prod.name}</div>
-          <div className="flex text-sm md:text-base xl:text-lg 2xl:text-xl items-center">
-            <LiaRupeeSignSolid className="w-[14px] h-[14px]" /> {prod.price}
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+
+          <div className="text-sm md:text-base 2xl:text-lg">{prod.name}</div>
+          <div className="flex text-sm md:text-base 2xl:text-lg  items-center">
+            <LiaRupeeSignSolid className="w-[14px] h-[14px] lg:w-4 lg:h-4" /> {prod.price}
+          </div>
+          </div>
+          <div className="flex gap-2 items-center h-fit md:hidden">
+            <div className="border flex items-center justify-around h-fit py-0 w-20">
+              <span className="cursor-pointer">-</span>
+              <span className="text-base 2xl:text-lg">{prod.quantity}</span>
+              <span className="cursor-pointer">+</span>
+            </div>
+            <RiDeleteBin6Line className="" />
           </div>
         </div>
       </div>
-      <div className="w-3/12 px-2">
+      <div className="hidden md:flex md:w-4/12 lg:w-3/12 px-2 gap-2 items-center h-fit">
         <div className="border flex items-center justify-around h-fit py-1 w-16 md:w-20 lg:w-24">
-          <span className="cursor-pointer">
-           -
-          </span>
-          <span>{prod.quantity}</span>
+          <span className="cursor-pointer">-</span>
+          <span className="text-base 2xl:text-lg">{prod.quantity}</span>
           <span className="cursor-pointer">+</span>
         </div>
+        <RiDeleteBin6Line className="" />
       </div>
-      <div className="w-2/12 flex items-center h-fit ">
-        <LiaRupeeSignSolid className="w-[14px] h-[14px]" /> <span className="xl:text-lg 2xl:text-xl">{prod.price}</span>
+      <div className="w-4/12 md:w-2/12 flex items-center h-fit ">
+        <LiaRupeeSignSolid className="w-[14px] h-[14px] lg:w-4 lg:h-4" />{" "}
+        <span className="xl:text-lg 2xl:text-xl">{prod.price}</span>
       </div>
     </div>
   );
--- src/app/myCart/page.tsx
+++ src/app/myCart/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 import CartProduct from "@/Components/CartProduct/CartProduct";
+import { Assistant } from "next/font/google";
 import Link from "next/link";
 import React, { useState } from "react";
 import { LiaRupeeSignSolid } from "react-icons/lia";
+
+const assistant = Assistant({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 function Page() {
   const [cart, setCart] = useState([
@@ -10,30 +16,30 @@ function Page() {
       name: "Ragi Milk Powder",
       price: 14499,
       image: "https://drive.google.com/file/d/1uKw2Cm7ZQ7TBVdYZVBFCGeytMTxWkkcN/view?usp=sharing",
-      quantity: 0,
+      quantity: 1,
       _id:1
     },
     {
       name: "Ragi Milk Powder",
       price: 299,
       image: "https://drive.google.com/file/d/1uKw2Cm7ZQ7TBVdYZVBFCGeytMTxWkkcN/view?usp=sharing",
-      quantity: 0,
+      quantity: 1,
       _id:2
     },
   ]);
 
   return (
-    <div className="w-full flex justify-center">
-    <div className="px-5 lg:px-10 py-2 min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-104px)] xl:min-h-[calc(100vh-120px)]   pageMountAnimation w-full max-w-[1850px]">
+    <div className={`${assistant.className} w-full flex justify-center mb-56`}>
+    <div className="px-7 md:px-28 lg:px-52 xl:px-72 2xl:px-80 py-2 min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-104px)] xl:min-h-[calc(100vh-120px)]   pageMountAnimation w-full max-w-[1850px]">
       <div className="flex justify-between py-10 items-center">
-        <div className="text-xl md:text-2xl lg:text-3xl 2xl:text-4xl font-semibold">Your Cart</div>
+        <div className="text-xl md:text-xl lg:text-2xl 2xl:text-3xl font-semibold">Your Cart</div>
         <div className="text-xs lg:text-sm underline font-semibold cursor-pointer hover:text-themeColorDark"><Link href='/'>Continue Shopping</Link></div>
       </div>
       <div className="flex flex-col">
         <div className="flex">
-          <div className="w-7/12 md:text-lg lg:text-xl 2xl:text-2xl">Product</div>
-          <div className="w-3/12 px-1 md:text-lg lg:text-xl 2xl:text-2xl">Quantity</div>
-          <div className="w-2/12 md:text-lg lg:text-xl 2xl:text-2xl">Total</div>
+          <div className="w-8/12 md:w-6/12 lg:w-7/12 lg:text-lg 2xl:text-xl">Product</div>
+          <div className="hidden md:block md:w-4/12 lg:w-3/12 px-1 lg:text-lg 2xl:text-xl">Quantity</div>
+          <div className="w-4/12 md:w-2/12 lg:text-lg 2xl:text-xl">Total</div>
         </div>
         <hr className="mb-2" />
 
@@ -42,11 +48,12 @@ function Page() {
         })}
         <hr className="mt-2" />
       </div>
-      <div className="flex">
-        <div className="hidden lg:block w-6/12 lg:w-10/12"></div>
-        <div className="w-full lg:w-2/12 flex flex-col gap-2 items-center lg:items-end">
-          <div className="flex items-center mt-3 lg:mt-0 lg:text-lg 2xl:text-xl"><span className="text-base lg:text-lg 2xl:text-xl mr-2">Estimated Total : </span><LiaRupeeSignSolid className="w-[14px] h-[14px]" /> 123000</div>
-          <div className="bg-themeColorDark text-milkWhite px-5 py-2 text-center rounded-lg font-semibold w-fit 2xl:text-xl">Checkout</div>
+      <div className="flex mt-5 lg:mt-0">
+        <div className="hidden lg:block lg:w-7/12"></div>
+        <div className="w-full lg:w-5/12 flex flex-col gap-4 items-center lg:items-end">
+          <div className="flex items-center mt-3 lg:text-lg 2xl:text-xl"><span className="text-base 2xl:text-lg mr-2">Estimated Total : </span><LiaRupeeSignSolid className="w-[14px] h-[14px] lg:w-4 lg:h-4" /> 123000</div>
+          <div className="text-xs">Shipping charges calculated at checkout</div>
+          <div className="bg-themeColorDark text-milkWhite px-14 lg:px-24  py-2 text-center rounded-sm font-semibold w-fit 2xl:text-xl">Checkout</div>
         </div>
       </div>
     </div>
