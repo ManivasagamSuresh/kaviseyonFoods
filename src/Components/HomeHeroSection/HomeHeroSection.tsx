32735b1bf14b40e71c9a6c6863a8430abfd05f41
Manivasagam Suresh <mani@ayoo.care>
2024-09-19 23:54:08 +0530

    hero section modified

--- src/Components/HomeHeroSection/HomeHeroSection.tsx
+++ src/Components/HomeHeroSection/HomeHeroSection.tsx
@@ -10,7 +10,7 @@ function HomeHeroSection() {
         <div className="w-full lg:h-[calc(100vh-144px)] xl:h-[calc(100vh-170px)] relative hidden lg:block bg-themeColorDark border ">
             {/* <Image src={'/Images/mainBanner.jpeg'} alt='' fill /> */}
         </div>
-        <div className="w-full h-[calc(100vh-144px)]  relative lg:hidden bg-themeColorLight border">
+        <div className="w-full h-[calc(100vh-172px)]  relative lg:hidden bg-themeColorLight border">
             <Image src={'/Images/mainB.png'} alt='' fill />
         </div>
     </div>
