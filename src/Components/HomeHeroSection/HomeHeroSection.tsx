815b44a61c837e3777b40e391f5060a26f26e5fa
Manivasagam Suresh <mani@ayoo.care>
2024-09-24 23:09:32 +0530

    hero section image removed

--- src/Components/HomeHeroSection/HomeHeroSection.tsx
+++ src/Components/HomeHeroSection/HomeHeroSection.tsx
@@ -10,8 +10,8 @@ function HomeHeroSection() {
         <div className="w-full lg:h-[calc(100vh-144px)] xl:h-[calc(100vh-170px)] relative hidden lg:block bg-themeColorDark border ">
             {/* <Image src={'/Images/mainBanner.jpeg'} alt='' fill /> */}
         </div>
-        <div className="w-full h-[calc(100vh-180px)] relative lg:hidden bg-themeColorLight border">
-            <Image src={'/Images/mainB.png'} alt='' fill />
+        <div className="w-full h-[calc(100vh-180px)] relative lg:hidden bg-themeColorDark border">
+            {/* <Image src={'/Images/mainB.png'} alt='image' fill /> */}
         </div>
     </div>
   )
