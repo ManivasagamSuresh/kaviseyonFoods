1ea3ef23da10a2ee1effa53f93c0180935e52779
Manivasagam Suresh <mani@ayoo.care>
2024-09-26 14:03:58 +0530

    style modified

--- src/Components/HomeHeroSection/HomeHeroSection.tsx
+++ src/Components/HomeHeroSection/HomeHeroSection.tsx
@@ -10,7 +10,7 @@ function HomeHeroSection() {
         <div className="w-full lg:h-[calc(100vh-244px)] xl:h-[calc(100vh-210px)] relative hidden lg:block bg-themeColorDark shadow-md">
             <Image src={'/Images/mainBannerDesktop.png'} alt='' fill />
         </div>
-        <div className="w-full h-[580px] sm:h-[860px] md:h-[90px] relative lg:hidden bg-themeColorDark ">
+        <div className="w-full h-[580px] sm:h-[860px] md:h-[960px] relative lg:hidden bg-themeColorDark ">
             <Image src={'/Images/mainBannerMobile.png'} alt='image' fill />
         </div>
     </div>
